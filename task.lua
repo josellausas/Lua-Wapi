d5e36dd02a6eba443de791de2885693aca5ca9b8
josellausas <jose@josellausas.com>
2015-12-17 00:39:51 -0600

    Fast commit

--- task.lua
+++ task.lua
@@ -1,8 +1,16 @@
 local Model = require("lapis.db.model").Model
 local Tasks = Model:extend("tasks")
-local Targets = Model:extend("targets")
+local Targets = Model:extend("targets",{
+	relations = {
+    	{"contact", belongs_to = "contacts"},
+    	{"establishment", belongs_to = "establisments"}
+
+  	}
+})
 local Jornadas = Model:extend("jornadas")
 local Actions = Model:extend("actions")
+local Contacts = Model:extend("contacts")
+local Establishments = Model:extend("establishments")
 
 
 local task = {}
@@ -15,6 +23,18 @@ task.allForUser = function(userObj)
 	Targets:include_in( myTasks, "target",  {as = "target"})
 	Jornadas:include_in(myTasks, "jornada", {as = "jornada"})
 	Actions:include_in(myTasks,  "action",  {as="action"})
+
+	local targets = {}
+	for i,t in pairs(myTasks)
+		if (t.target.type == "CONTACT") then
+			t.target.contact = t.target.get_contact()
+		else
+			t.target.establishment = t.target.get_establishment()
+		end
+	end
+			
+
+
 
 	return myTasks
 	
