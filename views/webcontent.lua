736d039f9fd5207ae623994381daf46acd3c7a44
josellausas <jose@josellausas.com>
2016-03-28 01:13:06 -0600

    Added more cositas

---
+++ views/webcontent.lua
@@ -0,0 +1,8 @@
+local webcontent = {
+	title = "Zunware",
+	description = "Zunware Apps| Mobile and Tech Integrations",
+	keywords = "business intelligence app, iot app, mobile BI, best business intelligence app, mobile developer, specialty apps, iot, internet of things, iot app, SAS app,mqtt app, real time business intelligence, real time, business intelligence, business inteligence",
+	author = "Zunware",
+}
+
+return webcontent
