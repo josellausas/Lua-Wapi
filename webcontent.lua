e22c2c3797032e3342525429ca8d940d1e76c694
josellausas <jose@josellausas.com>
2016-04-07 19:34:33 -0500

    Dynamic things are coming

--- webcontent.lua
+++ webcontent.lua
@@ -5,7 +5,16 @@ local webcontent = {
 	pages = {
 		index = {
 			header = "Intelligence in your pocket...",
-			description = [[Data moves at light speed! If you can't visualize and use IT in real time, your're too slow. Smart decisions are made in real time with real data. "The power to run your business with the touch of a finger!"]],
+			description = [[Data moves at light speed! Visualize and use IT in real time! or your're too slow. Smart decisions are made in real time with real data. "The power to run your business with the touch of a finger!"]],
+			ios_url = "#",
+			android_url = "/static/app-debug.apk",
+			carousel = {
+				"/img/device-slider/screen01.png",
+				"/img/device-slider/screen02.png",
+				"/img/device-slider/screen01.png",
+				"/img/device-slider/screen04.png"
+			},
+
 		},
 	},
 
