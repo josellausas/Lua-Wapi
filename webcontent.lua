3e90f1bf12d9a559ad6c664415960f8f31f84a16
josellausas <jose@josellausas.com>
2016-04-07 19:45:34 -0500

    More refactors

--- webcontent.lua
+++ webcontent.lua
@@ -1,3 +1,4 @@
+--[[ This describes a local website ]]
 local webcontent = {
 	title = "Llau Admin Systems",
 	description = "An administration site",
@@ -6,7 +7,7 @@ local webcontent = {
 		index = {
 			header = "Intelligence in your pocket...",
 			description = [[Data moves at light speed! Visualize and use IT in real time! or your're too slow. Smart decisions are made in real time with real data. "The power to run your business with the touch of a finger!"]],
-			ios_url = "#",
+			ios_url = "/#",
 			android_url = "/static/app-debug.apk",
 			carousel = {
 				"/img/device-slider/screen01.png",
@@ -14,10 +15,24 @@ local webcontent = {
 				"/img/device-slider/screen01.png",
 				"/img/device-slider/screen04.png"
 			},
-
+			social = {
+				twitter = {
+					url = "/#",
+					class = "sb-twitter",
+					icon = "fa fa-twitter",
+					title = "Twitter",
+					num = 13,
+				},
+				facebook = {
+					url = "/#",
+					class = "sb-facebook",
+					icon = "fa fa-facebook",
+					title = "Facebook",
+					num = 37,
+				},
+			},
 		},
 	},
-
 }
 
 return webcontent
