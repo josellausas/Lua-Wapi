b83f30012a322d6c8f6443024d837a65a42fd8f2
josellausas <jose@josellausas.com>
2016-04-07 19:17:34 -0500

    Changed some things

--- webcontent.lua
+++ webcontent.lua
@@ -2,6 +2,12 @@ local webcontent = {
 	title = "Llau Admin Systems",
 	description = "An administration site",
 	author = "Zunware",
+	pages = {
+		index = {
+			header = "Intelligence in your pocket...",
+			description = [[Data moves at light speed! If you can't visualize and use IT in real time, your're too slow. Smart decisions are made in real time with real data. "The power to run your business with the touch of a finger!"]],
+		},
+	},
 
 }
 
