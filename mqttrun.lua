ed0b5a37869c3ecba06249eb121a1e99c2924041
josellausas <jose@josellausas.com>
2016-05-07 23:36:32 -0500

    Oki

--- mqttrun.lua
+++ mqttrun.lua
@@ -5,13 +5,10 @@ print("Trying to start MQTT handler")
 -- Exit the loop if we error:
 local error_message = nil
 
-while (error_message == nil) do
-  -- This is the loop mr.
-  error_message = MQTTHandler:start()
-  socket.sleep(1.0)  -- seconds
-  -- print("MQTT alive")
-end
+error_message = MQTTHandler:start()
 
 if(error_message) then
 	print(error_message)
 end
+
+print("Got to the end, wtf")
